65a151d0171de7dd9a3c38ba4e63e379b1d1d1d1
edwinlayme <everedwinlayme@gmail.com>
2024-01-28 20:24:40 -0400

    modify

--- matches.js
+++ matches.js
@@ -163,15 +163,14 @@ function createMatchElements(matchData) {
   return team;
 }
  function createImage(teamData) {
-  const defaultTeamSrc = 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEibBBDPDNz7dyO9xcO6BjNbxzaIja9uehuCm3OoNkeaLGArmzElvpIP7fBp4Q34iOtyHcHAELbnmxOih4HZzKIUJqyW-k7MLK5EwoO23yoVxJRG3eLx8fpE6V9PCpXPZOjmuS2rSdQ8k4VHnf3nJX05M0ZerbdDnGOG0YmVncuAm2ABLrvYhQWSTQBzp5cQ/s320/team_shield_a.webp';
   const img = createElement("img", "team-logo");
 
-  img.src = teamData.logo || defaultTeamSrc;
+  img.src = teamData.logo;
   img.alt = teamData.name;
   img.width = 24; 
   img.height = 24;
 
-  return img
+  return img;
 }
 
 function createName(teamData) {
@@ -221,7 +220,7 @@ function parseApiResponse(match) {
   const [hour, minutes] = match.event_time.split(":");
   const leagueLogo = match.league_logo ? match.league_logo : match.country_logo;
   const homeTeamLogo =  match.home_team_logo.status !==404  ? match.home_team_logo : defaultTeamSrc;
-  const awayTeamLogo =  match.away_team_logo.status   !==404 ? match.away_team_logo : defaultTeamSrc;
+  const awayTeamLogo =  match.away_team_logo.status !==404 ? match.away_team_logo : defaultTeamSrc;
   return {
     key:match.event_key,
     leaguekey:match.league_key,
@@ -368,7 +367,7 @@ async function getMatches(date) {
   const matches = [];
   const apiKey = "3aade9eedc75fcad8ca4474270a52fe08cb8cccea5bfda3b684d244c07f91a24";
   const apiUrl = `https://apiv2.allsportsapi.com/football/?met=Fixtures&APIkey=${apiKey}&from=${getDateString(date)}&to=${getDateString(date)}`;
-  const response = await fetch(apiUrl)
+  const response = await fetch(apiUrl);
 
   const data = await response.json();
 
