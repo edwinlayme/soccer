3dd39f59e19ac12c79024b4030ab01874ea96984
edwinlayme <everedwinlayme@gmail.com>
2024-02-08 14:40:12 -0400

    update

---
+++ views/modal/component/Match/LineUps.js
@@ -0,0 +1,8 @@
+import { createElement } from "../../../../functions/common/common.js"
+
+const addLineUps = () =>{
+ const lineupsContainer = createElement("div","modal-lineups");
+return lineupsContainer;
+}
+
+export default addLineUps
